c1e2741b394296ec1643470c9500048bef4aaea7
manishtarjan2 <manishtarjan2@users.noreply.github.com>
2026-09-21 22:24:34 +0530

    fix: remove obsolete Next.js eslint config

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const nextConfig = {
     ],
   },
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '_mongodb._tcp.cluster0.v3p7nue.mongodb.net', 'localhost', '_mongodb._tcp.cluster0.v3p7nue.mongodb.net', 'snapkart-rust.vercel.app'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
